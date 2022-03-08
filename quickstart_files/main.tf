39e04f85eb9a863185724021cd655ec9bc444902
Brian Spendolini <brian.spendolini@oracle.com>
2022-03-08 11:35:34 -1000

    v1.0

--- quickstart_files/main.tf
+++ quickstart_files/main.tf
@@ -32,12 +32,48 @@ resource "oci_database_autonomous_database" "quickstart_autonomous_database" {
         is_free_tier ="true"
         license_model = "LICENSE_INCLUDED"
         #is_access_control_enabled = "false"
+        is_mtls_connection_required = "false"
+        whitelisted_ips = ["0.0.0.0/0"]
         is_auto_scaling_enabled = "false"
         is_data_guard_enabled = "false"
         is_dedicated = "false"
         is_preview_version_with_service_terms_accepted = false   
 }
 
+resource "oci_kms_vault" "demo_vault" {
+    #Required
+    compartment_id = var.compartment_ocid
+    display_name = "demoVault"
+    vault_type = "DEFAULT"
+}
+
+resource "oci_kms_key" "demo_key" {
+  #Required
+  compartment_id      = var.compartment_ocid
+  display_name        = "demoKey"
+  management_endpoint = oci_kms_vault.demo_vault.management_endpoint
+
+  key_shape {
+    #Required
+    algorithm = "AES"
+    length    = 32
+  }
+}
+
+resource "oci_vault_secret" "demo_secret" {
+    #Required
+    compartment_id = var.compartment_ocid
+    secret_content {
+        #Required
+        content_type = "BASE64"
+
+        #Optional
+        content = "${base64encode(random_string.password.result)}"
+    }
+    key_id = oci_kms_key.demo_key.id
+    secret_name = "quickstartPassword"
+    vault_id = oci_kms_vault.demo_vault.id
+}
 
 data "oci_database_autonomous_database" "quickstart_autonomous_database" {
     #Required
@@ -60,6 +96,34 @@ resource "local_file" "autonomous_data_warehouse_wallet_file" {
   
 }
 
+resource "oci_database_tools_database_tools_connection" "test_database_tools_connection" {
+    #Required
+    compartment_id = var.compartment_ocid
+    display_name = "QUIKDB DB Tools Connection"
+    type = "ORACLE_DATABASE"
+    connection_string = oci_database_autonomous_database.quickstart_autonomous_database.connection_strings.0.profiles.0.value
+
+    related_resource {
+        #Required
+        entity_type = "AUTONOMOUSDATABASE"
+        identifier = oci_database_autonomous_database.quickstart_autonomous_database.id
+    }
+    user_name = "quickstart"
+    user_password {
+        #Required
+        value_type = "SECRETID"
+
+        #Optional
+        secret_id = oci_vault_secret.demo_secret.id
+    }
+
+depends_on = [
+    local_file.autonomous_data_warehouse_wallet_file,
+    oci_vault_secret.demo_secret
+  ]
+
+}
+
 resource "null_resource" "sqlcl-load-data" {
 
         provisioner "local-exec" {    
@@ -72,7 +136,8 @@ resource "null_resource" "sqlcl-load-data" {
         }
 
 depends_on = [
-    local_file.autonomous_data_warehouse_wallet_file,
+    oci_database_tools_database_tools_connection.test_database_tools_connection,
+    local_file.autonomous_data_warehouse_wallet_file
   ]
 
 }

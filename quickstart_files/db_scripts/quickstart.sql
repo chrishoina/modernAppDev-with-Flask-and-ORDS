a1e88723216c9cbaae7ff9a11073e811b2b3fe1e
Brian Spendolini <brian.spendolini@oracle.com>
2022-03-08 12:01:36 -1000

    v1.0

--- quickstart_files/db_scripts/quickstart.sql
+++ quickstart_files/db_scripts/quickstart.sql
@@ -2,12 +2,13 @@
 /
 @./db_scripts/rest.sql
 /
+@./db_scripts/auto_rest.sql
+/
 conn quickstart/QUICK11##11vue@QUIKDB_high
 
 @./db_scripts/emp_create.sql
 
 @./db_scripts/emp_popul.sql
-/
 
 @./db_scripts/new_password.sql
 

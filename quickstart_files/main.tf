07af54c0af636254383bde1b7d85c8feaf678f94
Brian Spendolini <brian.spendolini@oracle.com>
2022-03-08 12:06:03 -1000

    v1.0

--- quickstart_files/main.tf
+++ quickstart_files/main.tf
@@ -142,7 +142,11 @@ depends_on = [
 
 }
 
+output "rest_get_url" {
 
+  value = replace("${oci_database_autonomous_database.quickstart_autonomous_database.connection_urls.0.sql_dev_web_url}quickstart/employees/","sql-developer","")
+
+}
 
 output "password" {
   value = "The password is ${random_string.password.result}"

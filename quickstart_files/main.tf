49145c3ffb4090a69fa5547bcf59844527250492
Brian Spendolini <brian.spendolini@oracle.com>
2022-03-08 12:14:24 -1000

    v1.0

--- quickstart_files/main.tf
+++ quickstart_files/main.tf
@@ -150,7 +150,7 @@ output "rest_get_url" {
 
 output "sdw_url" {
 
-  value = replace("${oci_database_autonomous_database.strava_autonomous_database.connection_urls.0.sql_dev_web_url}quickstart/sign-in/?username=QUICKSTART&r=_sdw%2F","sql-developer","")
+  value = replace("${oci_database_autonomous_database.quickstart_autonomous_database.connection_urls.0.sql_dev_web_url}quickstart/sign-in/?username=QUICKSTART&r=_sdw%2F","sql-developer","")
 
 }
 
